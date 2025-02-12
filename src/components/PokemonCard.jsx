41a5c5b979d86a6f412003c460cdcdc82b6e8908
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-02-12 12:38:45 -0500

    feat(pokemon-card): add click handler for interaction
    - Accept `onClick` prop to handle user interactions
    - Attach `onClick` event to the Pokémon card container
    - Enable interactivity for game mechanics

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,8 @@
 import '../styles/PokemonCard.css';
 
-function PokemonCard({ pokemon }) {
+function PokemonCard({ pokemon, onClick }) {
   return (
-    <div className="pokemon-card">
+    <div className="pokemon-card" onClick={onClick}>
       <img className="poke-img" src={pokemon.image} alt={pokemon.name} />
       <h3>{pokemon.name}</h3>
     </div>
